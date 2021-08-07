4b0cfe0fb91cc19971d3097fb70735c84ab36605
s.han <s.han@methodologic.co.jp>
2021-08-07 16:38:59 +0900

    commit

---
+++ src/views/Sample/Sample10.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="alert alert-primary">
+        <h1>{{ title }}</h1>
+        <pre v-on:click = "clear">{{ message }}</per>
+        <br>
+        <div>
+            <input type = "text"  v-on:keydown="type" 
+            class = "from-control"></div>     
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data: function(){
+        return {
+            title: 'Event',
+            message: '',
+        }
+    },
+    methods:{
+        type(event) {
+            this.message += event.key + ' '
+            if (event.key == "Escape") {
+                this.message = ''
+            }
+            event.target.value = ''
+        },
+        clear() {
+            this.message = ''
+        }
+    },
+}
+</script>
